3545586ccb2a086b99380c8a06ab966e57d38ef4
Birk Brauer <birk.brauer@erasys.de>
2012-10-17 14:02:48 +0200

    Mingle Service completed: now creating a new card should work.

--- mingle/src/main/java/mingleplugin/MingleRestService.java
+++ mingle/src/main/java/mingleplugin/MingleRestService.java
@@ -19,7 +19,7 @@ import org.kohsuke.stapler.QueryParameter;
  * Class for the Mingle connection using the API v2.
  *
  * @author Birk Brauer
- * @version 0.6
+ * @version 0.7
  */
 public class MingleRestService {
 /**
@@ -85,7 +85,7 @@ public class MingleRestService {
 /**
  * Gets a mingle card by it's unique number.
  *
- * @param number The unique number of the requested card.
+ * @param int number The unique number of the requested card.
  * 
  * @return MingleCard Returns a mingle card by it's unique number.
  */
@@ -104,9 +104,53 @@ public class MingleRestService {
  */
   public void updateCardByNumber(int number, MingleCard card) {
     // Converts a MingleCard to a XML String.
+
     String xml = xstream.toXML(card);
-    return card;
+    doMingleCall(generateRestUrl("cards/"+number+".xml"), "PUT", xml);
   }
+
+/**
+ * Creates a new empty card on the mingle server. The simple way, close to the Mingle API.
+ *
+ * @param String name The name of the new mingle card that should be created on the server.
+ * @param String cardtype The cardtype of the new mingle card that should be created on the server.
+ *
+ * @return URL The URL of the new created card will be returned.
+ */
+  public int createEmptyCard(String name, String cardtype) {
+    // generates XML-string for card creation:
+    String xml = "<card><name>"+name+"</name><card_type_name>"+cardtype+"</card_type_name></card>";
+
+    // creates a new card:
+    URL url = new URL(doMingleCall(generateRestUrl("cards.xml"), "POST", xml));
+    
+    return url;
+  }
+
+/**
+ * Creates a new card on the mingle server.
+ *
+ * @param MingleCard card The new mingle card that should be created on the server.
+ *
+ * @return int The unique card number of the new created card will be returned.
+ */
+  public int createCard(MingleCard card) {
+    // creates a new card:
+    URL url = createEmptyCard(card.getName(), card.getCardtype());
+    
+    // get cardnumber out of url:
+    String urlpath = url.getPath();
+    int cardnumber = (int)urlpath.substring(urlpath.lastIndexOf("/cards/")+7, urlpath.lastIndexOf(".xml"));
+    // int cardnumber = (int)urlpath.substring(urlpath.lastIndexOf("/cards/")+7, urlpath.lastIndexOf(".xml") - (urlpath.lastIndexOf("/cards/")+7));
+    
+    // updates the new created card with the passed content:
+    updateCardByNumber(cardnumber, card);
+
+    return cardnumber;
+  }
+
+  //TODO: Do we need: Method getListOfCards(view, page, filters[], sort, order, tagged_with ) and so on?
+
 
 /**
  * Performs a REST call on the mingle server and returns a XML string if we requested a ressource or a link
@@ -114,19 +158,22 @@ public class MingleRestService {
  *
  * @param url URL The url that represents the RESTful url for the mingle server.
  * @param method String method This will set the used HTTP method. Only GET, POST, PUT or DELETE are supported.
- * @param xml String A string that represents a MingleObject in XML form.
+ * @param xml String A string that represents a MingleObject in XML form or null if just requesting data.
  * 
  * @return String XML object that can be parsed as a MingleObject if this was requested or a URL to the 
  *                ressource which has just been updated.
  */
-// TODO: doesn't accept input yet!
   public String doMingleCall(URL url, String method, String xml) {
+    // Default HTTP method is GET:
     if (method == null) method = "GET";
+
+    // Set up connection:
     //HttpURLConnection connection = new HttpURLConnection(url);
     HttpURLConnection connection = (HttpURLConnection)url.openConnection();
     connection.setDoInput(true);
     if (xml != null) connection.setDoOutput(true);
-    else method = "GET"; // if nothing to update is given it's obviously that we should use GET
+    else method = "GET"; // if nothing to update/delete is given it's obviously that we should use GET
+
     // Checks for valid Http method. Mingle supports: GET, POST, PUT or DELETE.
     if (method == "GET" || method == "POST" || method == "PUT" || method == "DELETE") {
       connection.setRequestMethod(method);
@@ -154,6 +201,16 @@ public class MingleRestService {
         resultString = "";
       }
     }
+
+    /**
+     * "Result: If you were authorized to perform the operation, and the operation succeeded, 
+     * you will be returned a LOCATION ATTRIBUTE in the http header of the response, which 
+     * is a URL to the updated resource."
+     */
+
+    //get the LOCCATION ATTRIBUTE from the HTTP Header:
+    if (resultString == "") resultString = connection.getHeaderFieldDate("Location");
+
     return resultString;
   }
 
