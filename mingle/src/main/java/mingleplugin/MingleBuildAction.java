2aca02d66a3818d4c3caa1edcfbe5c5b97729592
Birk Brauer <birk.brauer@erasys.de>
2012-11-13 14:00:29 +0100

    new builder file

---
+++ mingle/src/main/java/mingleplugin/MingleBuildAction.java
@@ -0,0 +1,58 @@
+package mingleplugin;
+
+import hudson.model.AbstractBuild;
+import hudson.model.Action;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+* Mingle cards related to the build.
+*
+* @author Birk Brauer
+*/
+public class MingleBuildAction implements Action {
+    public final AbstractBuild<?, ?> owner;
+
+    public MingleCard[] cards;
+
+    public MingleBuildAction(AbstractBuild<?, ?> owner, Collection<MingleCard> cards) {
+        this.owner = owner;
+        this.cards = cards.toArray(new MingleCard[cards.size()]);
+        Arrays.sort(this.cards);
+    }
+
+    public String getIconFileName() {
+        return null;
+    }
+
+    public String getDisplayName() {
+        return "Mingle Builder stuff";
+    }
+
+    public String getUrlName() {
+        return "mingle";
+    }
+
+/**
+ * Finds {@link MingleCard} whose ID matches the given one.
+ */
+    public MingleCard getCard(int number) {
+        for (MingleCard card : cards) {
+            if(card.id.equals(id))
+                return card;
+        }
+        return null;
+    }
+
+    public void addCard(Set<MingleCard> cardToBeSaved) {
+        SortedSet<MingleCard> allCard = new TreeSet<MingleCard>();
+        allCard.addAll(cardToBeSaved);
+        allCard.addAll(Arrays.asList(this.cards));
+        
+        this.cards = allCard.toArray(new MingleCard[allCard.size()]);
+    }
+}
